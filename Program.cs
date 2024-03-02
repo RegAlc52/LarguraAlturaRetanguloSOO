a90c6c268cd564f9fe32e09a25c4886d3773d1ca
Regiane_de_Alcântara <any_any_2011@hotmail.com>
2024-03-02 20:08:09 -0300

    InicioProjeto

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿
+//Faça um programa que leia os valores 
+//a largura de um retângulo = double
+//a altura de um retangulo = double
+//Em seguida, mostrar na tela o valor de 
+//sua área, perímetro e diagonal. 
+//Informe se o valor é valido ou inválido
+//Solicite novamente a largura do retângulo
+//Solicite novamente a altura do retângulo
+// OBs:Verificar os calculos.
+// Um triangulo tem 
+//CalcularArea() : double, 
+//CalcularPerimetro: double
+//CalcularDiagonal(): double
+//conter os Métodossing System;using System;
+//Sem Orientação a Objetos
